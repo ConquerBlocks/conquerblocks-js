e04317c28da2fd0aa26bb9552cede81de71ea5c4
Bienvenido Sáez Muelas <bienvenidosaez@gmail.com>
2023-12-15 17:46:54 +0100

    Añadiendo ejemplos de la clase 5

---
+++ 01_clases/Clase 05 - Sentencias de control de flujo, number y string/code/ejemplos.js
@@ -0,0 +1,28 @@
+// Creación de Strings
+// Comillas simples
+// Comillas dobles
+// Backtips
+
+// Concatenación con +
+// Concatenación con template literals
+// Concatenación de array con join()
+// Concatenación con concat
+
+// Cómo escapar caracteres, por ejemplo ' o ". Alternando comillas o con caracter de escape o template literals
+
+// Strings largos con saltos de línea
+
+// strings primitivos vs strings objects, son inmutables
+// const stringPrimitivo = 'Soy un string primitivo'
+// console.log(typeof stringPrimitivo);
+
+// const stringObjeto = new String('Hola soy un string de tipo objeto')
+// console.log(typeof stringObjeto);
+
+
+// Métodos de string
+
+
+let cadena = 'Hola soy una cadena'
+cadena.length
+cadena.includes(¡ca¡)
